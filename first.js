0fa2ca10f9991c8ca32888f86e1b2ca9d0d8868e
Diksha-1 <bansal.diksha1234@gmail.com>
2020-09-28 10:06:19 +0530

    first js project

---
+++ first.js
@@ -0,0 +1,26 @@
+function login()
+	{
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("please enter user name.");
+		}
+		else if(pwd=='')
+		{
+        	alert("enter the password");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Enter valid email id.");
+		}
+		else if(pwd.length < 8 || pwd.length > 8)
+		{
+			alert("Password min and max length is 8.");
+		}
+		else
+		{
+	alert('Thank You for Login');
+			}
+	}
